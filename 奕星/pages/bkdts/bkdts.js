6dfc3da736598988129046a3043e4584ebd734af
YaoZhijiang6 <1129496077@qq.com>
2018-12-28 11:15:58 +0800

    修改项目名

---
+++ 奕星/pages/bkdts/bkdts.js
@@ -0,0 +1,36 @@
+// bkdts.wxml
+Page({
+  data:{
+    bookslst:[]
+  },
+  onLoad: function (options){
+    console.log(options.showBooks)
+    var lit = JSON.parse(options.showBooks);
+    console.log(lit)
+    this.setData({
+      bookslst:lit
+    })
+    console.log(lit)
+  },
+  xq:function(e){
+    console.log(e.currentTarget.dataset.id)
+    var id=e.currentTarget.dataset.id
+    wx.request({
+      url: 'http://localhost:8080/bookx',
+      data: {bid:id},
+      header: { 'content-type': 'application/x-www-form-urlencoded;charset=utf-8'},
+      method: 'post',
+      dataType: 'json',
+      responseType: 'text',
+      success: function(res) {
+        console.log(res)
+        var bok=JSON.stringify(res.data)
+        wx.navigateTo({
+          url: '../bookdetails/bookdetails?obj='+bok,
+        })
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  }
+})
--- 奕星/pages/index/index.js
+++ 奕星/pages/index/index.js
@@ -1,54 +1,241 @@
-//index.js
 //获取应用实例
-const app = getApp()
+var app = getApp()
 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    tyname: [],
+    cx:[],
+    imgUrls: [
+      '../../img/rotation_chart/1.jpg',
+      '../../img/rotation_chart/2.jpg',
+      '../../img/rotation_chart/3.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  //获取当前滑块的index
+  bindchange: function (e) {
+    // 获取分类
+    const that = this;
+    console.log(e)
+    if (e.detail.current==1){
+      var list = this;
+    wx.request({
+      url: 'http://localhost:8080/showType',
+      method: "post",
+      data: {
+
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        console.log(res.data)
+        list.setData({
+          tyname: res.data
         })
       }
+    })
+    }
+    that.setData({
+      currentData: e.detail.current
+    })
+  },
+  //点击切换，滑块index赋值
+  checkCurrent: function (e) {
+    const that = this;
+    if (that.data.currentData === e.target.dataset.current) {
+      return false;
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+      that.setData({
+        currentData: e.target.dataset.current
       })
     }
   },
-  getUserInfo: function(e) {
+  //推荐
+  recommend: function (e) {
+    var thiss = this
+    wx.request({
+      url: 'http://localhost:8080/home_page',
+      method: 'GET',
+      data: {
+        type: '推荐'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        thiss.setData({
+          items: res.data
+        })
+      }
+    })
+  },
+  //排行
+  ranking: function (e) {
+    var thiss = this
+    wx.request({
+      url: 'http://localhost:8080/home_page',
+      method: 'GET',
+      data: {
+        type: '排行'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        thiss.setData({
+          items: res.data
+        })
+      }
+    })
+  },
+  //男生
+  boy: function (e) {
+    var thiss = this
+    wx.request({
+      url: 'http://localhost:8080/home_page',
+      method: 'GET',
+      data: {
+        type: '男'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        thiss.setData({
+          items: res.data
+        })
+      }
+    })
+  },
+  //女生
+  girl: function (e) {
+    var thiss = this
+    wx.request({
+      url: 'http://localhost:8080/home_page',
+      method: 'GET',
+      data: {
+        type: '女'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        thiss.setData({
+          items: res.data
+        })
+      }
+    })
+  },
+  //首页书籍跳转
+  tiaozhuan: function (e) {
+    console.log(132)
+  },
+  // 那个类型的书籍
+    thisType: function (e) {
+    var tyn = e.currentTarget.dataset.id
+    wx.request({
+      url: 'http://localhost:8080/booksType',
+      data: {
+        type: tyn
+      },
+      method: "post",
+      header: {
+        'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+      },
+      success: function (res) {
+        var books = JSON.stringify(res.data)
+        wx.navigateTo({
+          url: '../bkdts/bkdts?showBooks=' + books,
+        })
+        // console.log(books)
+      }
+    })
+  },
+  // 畅销
+  shopp:function(e){
     console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+    var that=this;
+    wx.request({
+      url: 'http://localhost:8080/shopp',
+      data: '',
+      header: {
+        'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+      },
+      method: 'post',
+      dataType: 'json',
+      responseType: 'text',
+      success: function(res) {
+        console.log(res)
+        that.setData({
+          cx:res.data
+        })
+      },
+      fail: function(res) {},
+      complete: function(res) {},
     })
   }
 })
