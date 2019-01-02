bbf9806513440fa0a00c69bfaa5fc2d0a689beda
zhuyanping <1515108997@qq.com>
2019-01-02 18:38:08 +0800

    zyp

--- 奕星/pages/index/index.js
+++ 奕星/pages/index/index.js
@@ -1,6 +1,6 @@
 //获取应用实例
 var app = getApp()
-
+var count = 1;
 Page({
 
   /**
@@ -59,15 +59,15 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    wx.showLoading({
-      title: '玩命加载中',
-      mask: true,
-      success: function(res) {
-       wx.stopPullDownRefresh()
-      },
-      fail: function(res) {},
-      complete: function(res) {},
-    })
+    // wx.showLoading({
+    //   title: '玩命加载中',
+    //   mask: true,
+    //   success: function(res) {
+    //    wx.stopPullDownRefresh()
+    //   },
+    //   fail: function(res) {},
+    //   complete: function(res) {},
+    // })
   },
 
   /**
@@ -227,6 +227,7 @@ Page({
   shopp:function(e){
     console.log(e)
     var that=this;
+    that.data.cx.concat(0)
     wx.request({
       url: 'http://localhost:8080/shopp',
       data: '',
@@ -238,9 +239,8 @@ Page({
       responseType: 'text',
       success: function(res) {
         console.log(res)
-        that.setData({
-          cx:res.data
-        })
+          that.setData({
+            cx: res.data})
       },
       fail: function(res) {},
       complete: function(res) {},
@@ -248,28 +248,42 @@ Page({
   },
   // 人气
   heat:function(){
-    var count=1;
+
+    wx.showLoading({
+      title: '玩命加载中',
+    })
+    console.log(count)
     var that = this;
     wx.request({
       url: 'http://localhost:8080/heat',
-      data: '',
+      data: {
+        pageNum:count
+      },
       header: { 'content-type': 'application/x-www-form-urlencoded;charset=utf-8'},
-      method: 'post',
+      method: 'POST',
       dataType: 'json',
       responseType: 'text',
       success: function(res) {
-        console.log(res)
-        that.setData({
-          cx: res.data
-        })
+        // console.log(res.data.length)
+        count++
+          that.setData({
+            cx: that.data.cx.concat(res.data)
+          })
+         
+        wx.hideLoading();
       },
       fail: function(res) {},
       complete: function(res) {},
     })
   },
-  lower:function(){
-    
-  },
+  // lowers:function(){
+  //   // if(flge!=true){
+  //     wx.showLoading({
+  //       title: '玩命加载中',
+  //     })
+  //   // }
+  //   this.heat()
+  // },
   xq: function (e) {
     console.log(e.currentTarget.dataset.id)
     var id = e.currentTarget.dataset.id
